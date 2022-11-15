aac11f6678aa63b1ffefb23b15868e5e702410a7
Thomas Fisher <80178894+tfisher013@users.noreply.github.com>
2022-11-15 16:31:41 -0500

    Added Format And Quality Tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,25 @@
 using SuperStreetViewRouter
+using Aqua
+using Documenter
+using MyJuliaPackage
+using JuliaFormatter
 using Test
 
-@testset "SuperStreetViewRouter.jl" begin
-    # Write your tests here.
+DocMeta.setdocmeta!(
+MyJuliaPackage,
+:DocTestSetup,
+:(using MyJuliaPackage);
+recursive=true
+)
+
+@testset verbose = true "MyJuliaPackage.jl" begin
+@testset verbose = true "Code quality (Aqua.jl)" begin
+Aqua.test_all(MyJuliaPackage; ambiguities=false)
+end
+@testset verbose = true "Code formatting (JuliaFormatter.jl)" begin
+@test format(MyJuliaPackage; verbose=true, overwrite=false)
+end
+@testset verbose = true "Doctests (Documenter.jl)" begin
+doctest(MyJuliaPackage)
+end
 end
