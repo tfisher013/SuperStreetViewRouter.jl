223e0c4e7d7349b1b19307d506c5b239d18d5004
Thomas Fisher <80178894+tfisher013@users.noreply.github.com>
2022-11-28 19:42:43 -0500

    Removed export of deleted file

--- src/SuperStreetViewRouter.jl
+++ src/SuperStreetViewRouter.jl
@@ -7,7 +7,7 @@ using Statistics
 using DataStructures
 
 export benchmark_random_walk_solutions
-export get_total_city_cost, get_total_city_length, generate_output_file
+export get_total_city_cost, get_total_city_length
 export solve_graph_greedy, create_input_graph
 
 include("functions.jl")
