5005d941b19d26deeb831e825fd63185394b1142
Thomas Fisher <80178894+tfisher013@users.noreply.github.com>
2022-11-28 20:46:39 -0500

    Removed unused and inefficient function
    See https://github.com/tfisher013/SuperStreetViewRouter.jl/issues/7.

--- src/util.jl
+++ src/util.jl
@@ -1,33 +1,5 @@
 """
 
-    get_city_street(city::City, start_junction::Int64, end_junction::Int64) 
-
-Returns a Street object in the provided City that allows travel from the provided start
-junction index to the provided end junction index whether one-directional or bi-directional. 
-Returns nothing if no such Street is found.
-"""
-function get_city_street(city::City, start_junction::Int64, end_junction::Int64)
-    for street in city.streets
-        if street.endpointA == start_junction
-            if street.endpointB == end_junction
-                return street
-            end
-        end
-
-        if street.bidirectional
-            if street.endpointB == start_junction
-                if street.endpointA == end_junction
-                    return street
-                end
-            end
-        end
-    end
-
-    return nothing
-end
-
-"""
-
     get_total_city_cost(city::City)
 
 Returns the total time (in seconds) required to traverse all streets of the provided city.
