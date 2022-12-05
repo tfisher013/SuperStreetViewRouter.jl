9af79898d857df485b2498da7076130a54345dd2
luc paoli <39316550+lucpaoli@users.noreply.github.com>
2022-12-05 15:09:27 -0500

    Remove include of deleted file

--- src/SuperStreetViewRouter.jl
+++ src/SuperStreetViewRouter.jl
@@ -8,7 +8,6 @@ using Statistics
 export get_possible_streets, get_total_city_cost, get_total_city_length, solve_graph_greedy
 export create_input_graph
 
-include("functions.jl")
 include("graphs.jl")
 include("solver.jl")
 include("util.jl")
