1866f7be343ab2b103b2301354ba3b85f81c37ce
luc paoli <39316550+lucpaoli@users.noreply.github.com>
2022-12-05 15:04:43 -0500

    remove export

--- src/SuperStreetViewRouter.jl
+++ src/SuperStreetViewRouter.jl
@@ -5,7 +5,6 @@ using Graphs, SimpleValueGraphs
 using HashCode2014
 using Statistics
 
-export benchmark_random_walk_solutions
 export get_possible_streets, get_total_city_cost, get_total_city_length, solve_graph_greedy
 export create_input_graph
 
